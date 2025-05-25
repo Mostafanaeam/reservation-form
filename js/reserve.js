8f2e532dc537e3f2ff21e660d58602554fbfe904
Mostafa Naeam <mnaeam10@gmail.com>
2025-05-25 17:50:50 +0300

    refactor(reserve): improve date handling and clean up HTML/CSS
    - Fix date calculation logic in reserve.js to properly handle tomorrow's date
    - Clean up HTML structure by moving script tag outside body and updating title
    - Remove unused CSS import and add consistent formatting
    - Add console log for debugging current date

--- js/reserve.js
+++ js/reserve.js
@@ -1,21 +1,27 @@
 var currentDateTime = new Date();
-var year = currentDateTime.getFullYear();
-var month = (currentDateTime.getMonth() + 1);
-var date = (currentDateTime.getDate() + 1);
 
-if(date < 10) {
-  date = '0' + date;
+var year = currentDateTime.getFullYear();
+var month = currentDateTime.getMonth() + 1;
+var date = currentDateTime.getDate();
+var tomorrowDay = currentDateTime.getDate() + 1;
+
+if (date < 10) {
+  date = "0" + date;
 }
-if(month < 10) {
-  month = '0' + month;
+if (month < 10) {
+  month = "0" + month;
 }
 
 var dateTomorrow = year + "-" + month + "-" + date;
+var dateAfterTomorrow = year + "-" + month + "-" + tomorrowDay;
+
 var checkinElem = document.querySelector("#checkin-date");
 var checkoutElem = document.querySelector("#checkout-date");
 
 checkinElem.setAttribute("min", dateTomorrow);
 
 checkinElem.onchange = function () {
-    checkoutElem.setAttribute("min", this.value);
-}
+  checkoutElem.setAttribute("min", checkinElem.value);
+};
+
+console.log((currentDateTime));
